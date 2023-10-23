c3b92f8d26c5543154715e810f6c19604e4ed842
Farid <fa1rid@live.com>
2023-10-23 12:13:01 +0400

    Create appliances.js

---
+++ appliances.js
@@ -0,0 +1,175 @@
+const appliances = {
+    Residential: [
+        {
+            Category: "Kitchen",
+            Appliances: {
+                "Refrigerator": 150,
+                "Microwave": 1200,
+                "Toaster": 800,
+                "Blender": 400,
+                "Coffee Maker": 1000,
+                "Dishwasher": 1500,
+                "Food Processor": 600,
+                "Oven": 3500,
+            },
+        },
+        {
+            Category: "Laundry",
+            Appliances: {
+                "Washing Machine": 500,
+                "Dryer": 4000,
+                "Iron": 1500,
+                "Vacuum Cleaner": 800,
+                "Sewing Machine": 150,
+                "Humidifier": 200,
+            },
+        },
+        {
+            Category: "Entertainment",
+            Appliances: {
+                "TV": 200,
+                "Game Console": 150,
+                "Stereo System": 500,
+                "Home Theater": 1200,
+                "Projector": 800,
+                "Amplifier": 400,
+            },
+        },
+        {
+            Category: "Home Office",
+            Appliances: {
+                "Laptop": 200,
+                "Desktop Computer": 350,
+                "Printer": 600,
+                "Desk Lamp": 60,
+                "Scanner": 300,
+                "Shredder": 200,
+                "Router": 10,
+            },
+        },
+        {
+            Category: "Bedroom",
+            Appliances: {
+                "Air Conditioner": 2000,
+                "Fan": 75,
+                "Electric Blanket": 200,
+                "Alarm Clock": 5,
+                "Hair Dryer": 1200,
+            },
+        },
+        {
+            Category: "Fans Coolers",
+            Appliances: {
+                "Ceiling Fan": 75,
+                "Table Fan": 50,
+                "Air Cooler": 200,
+            },
+        },
+        {
+            Category: "Lights",
+            Appliances: {
+                "LED Bulb": 10,
+                "Incandescent Bulb": 60,
+                "Fluorescent Tube": 40,
+                "Desk Lamp": 20,
+            },
+        },
+        {
+            Category: "Home Appliances",
+            Appliances: {
+                "Vacuum Cleaner": 800,
+                "Water Heater": 1500,
+                "Refrigerator": 150,
+                "Microwave": 1200,
+            },
+        },
+        {
+            Category: "TV Entertainment",
+            Appliances: {
+                "TV": 200,
+                "Soundbar": 80,
+                "DVD Player": 35,
+                "Gaming Console": 150,
+            },
+        },
+        {
+            Category: "ACs",
+            Appliances: {
+                "Split AC": 1500,
+                "Window AC": 1200,
+                "Inverter AC": 1700,
+            },
+        },
+        {
+            Category: "Motors",
+            Appliances: {
+                "Ceiling Fan Motor": 75,
+                "Water Pump Motor": 750,
+                "Refrigerator Compressor": 300,
+            },
+        },
+    ],
+    Commercial: [
+        {
+            Category: "Kitchen",
+            Appliances: {
+                "Industrial Refrigerator": 1000,
+                "Commercial Microwave": 1800,
+                "Commercial Oven": 3500,
+                "Coffee Machine": 1200,
+                "Dishwasher": 2000,
+                "Deep Fryer": 2500,
+                "Griddle": 3500,
+            },
+        },
+        {
+            Category: "Office",
+            Appliances: {
+                "Desktop Computer": 250,
+                "Laptop": 200,
+                "Laser Printer": 600,
+                "Copier": 1500,
+                "Fax Machine": 300,
+                "Conference Phone": 500,
+                "Projector": 800,
+                "Security Camera": 20,
+            },
+        },
+        {
+            Category: "Industrial",
+            Appliances: {
+                "Conveyor Belt": 5000,
+                "Heavy Machinery": 10000,
+                "Industrial Oven": 6000,
+                "Welding Machine": 7000,
+                "Air Compressor": 4500,
+                "Generator": 8000,
+                "CNC Machine": 3500,
+            },
+        },
+        {
+            Category: "Healthcare",
+            Appliances: {
+                "MRI": 15000,
+                "X-Ray Machine": 8000,
+                "Ultrasound": 3000,
+                "Ventilator": 1800,
+                "Defibrillator": 1200,
+                "ECG Machine": 600,
+                "Medical Refrigerator": 250,
+            },
+        },
+        {
+            Category: "Hospitality",
+            Appliances: {
+                "Hotel TV": 250,
+                "Ice Maker": 800,
+                "Dish Warming Trolley": 1000,
+                "Espresso Machine": 1500,
+                "Commercial Washer": 3000,
+                "Commercial Dryer": 5000,
+                "Wine Cooler": 300,
+            },
+        },
+    ],
+};
